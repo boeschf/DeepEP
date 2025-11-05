c49ff523f8b5f3525cb477dcf1c3e092d358e86f
boeschf <48126478+boeschf@users.noreply.github.com>
2025-11-05 17:49:50 +0100

    run scripts

---
+++ run_intranode.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=csstaff
+#SBATCH --job-name=deepep-test
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=%x_01.log
+#SBATCH --uenv=pytorch/v2.8.0:rc24:/user-environment
+#SBATCH --view=default
+
+srun -ul --mpi=pmi2 bash -c "
+PYTHONPATH=\$(pwd) \
+NVSHMEM_DISABLE_CUDA_VMM=1 \
+NVSHMEM_SYMMETRIC_SIZE=4G \
+NVSHMEM_REMOTE_TRANSPORT=libfabric \
+MASTER_ADDR=\$(scontrol show hostnames \$SLURM_JOB_NODELIST | head -n 1) \
+MASTER_PORT=29500 \
+RANK=\${SLURM_NODEID} \
+WORLD_SIZE=\${SLURM_NTASKS} \
+.venv/bin/python tests/test_intranode.py --num-processes=4 --num-experts=256 --num-tokens=8192
+"
+
+#FI_MR_CACHE_MONITOR=userfaultfd NCCL_NET_GDR_LEVEL=PHB NCCL_NET="AWS Libfabric" LD_LIBRARY_PATH=/user-environment/env/default/lib64:/user-environment/env/default/lib/python3.12/site-packages/torch/lib  python tests/test_intranode.py --num-processes=4
